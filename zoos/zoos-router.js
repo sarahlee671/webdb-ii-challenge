3e9cf7bfc59f72b673e0a14b940a63ebecc3aa80
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-04 12:08:09 -0700

    added delete endpoint and returns the count or record deleted

--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -22,6 +22,30 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+  db('zoos')
+    .where({id: req.params.id})
+    .first()
+    .then(zoo => {
+      if (zoo) {
+        res.status(200).json(zoo);
+      } else {
+        res.status(404).json({ message: 'Zoo with that ID does not exist'})
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+})
+
+router.post('/', (req, res) => {
+  db('zoos').insert(req.body, 'ids').then(ids => {
+      res.status(201).json(req.body)
+  })
+  .catch(error => {
+    res.status(500).json(error)
+  })
+});
 
 router.put('/:id', (req, res) => {
   const changes = req.body;
@@ -30,10 +54,28 @@ router.put('/:id', (req, res) => {
   .update(changes)
   .then(zoo => {
       res.status(200).json({changes})
+
   })
   .catch(error => {
     res.status(500).json(error)
   })
+});
+
+router.delete('/:id', (req, res) => {
+  db('zoos')
+    .where({id: req.params.id})
+    .del()
+    .then(count => {
+      if (count > 0) {
+        const unit = count > 1 ? 'records': 'record';
+        res.status(200).json({ message: `${count} ${unit} deleted`})
+      } else {
+        res.status(404).json({ message: 'zoo with the ID is not found'})
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
 });
 
 
